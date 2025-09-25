680b6725a6ce753b63ceca5f8e984cd8014e0e6f
ELSiSi <arezk0550@gemail.com>
2025-09-25 15:20:54 +0300

    update index.js

---
+++ menu/menu.js
@@ -0,0 +1,41 @@
+let buttons = document.querySelectorAll('.menu .box button');
+let boxes = document.querySelectorAll('.menu .box');
+
+let lastActiveIndex = -1; // علشان نعرف الزر اللي اتضغط آخر مرة
+ 
+buttons.forEach(function (button , index) {
+  button.onclick = function () {
+    if (lastActiveIndex === index) {
+
+      button.style.cssText = `
+  background-color: var(--hover-color);
+`;
+     boxes[index].querySelector('img').style.display = "none";
+
+      lastActiveIndex =  -1;
+
+     }else if(lastActiveIndex !== index) {
+      buttons.forEach(function (bu , i){
+
+      bu.style.cssText = `
+  background-color: var(--hover-color);
+`;
+    boxes[i].querySelector('img').style.display = " none";
+
+
+      })
+      button.style.cssText = `
+  background-color: #2ecc71;
+`;
+ boxes[index].querySelector('img').style.cssText =  `
+  opacity: 1;
+  transform: scale(1);
+     transition:  10s  ;
+  display: inline-block;
+
+`;
+
+      lastActiveIndex = index;
+    }
+  };
+});
